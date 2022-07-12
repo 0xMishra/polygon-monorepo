8b9b47b1cc7cc3ee1804bb33b881d98076f6fdc1
mishra811 <mishrakundan073@gmail.com>
2022-07-12 13:35:18 +0530

    added tests and upload script

--- week-5/script/Ghost.s.sol
+++ week-5/script/Ghost.s.sol
@@ -2,11 +2,14 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import {Ghost} from "src/Ghost.sol";
 
-contract ContractScript is Script {
+contract GhostScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast();
+        new Ghost();
+        vm.stopBroadcast();
     }
 }
---
+++ week-5/test/Ghost.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/Ghost.sol";
+contract GhostTest is Test {
+    Ghost ghost;
+    function setUp() public {
+        ghost = new Ghost();
+    }
+
+    function testBalance() public {
+        uint value = ghost.balanceOf(msg.sender);
+        assertEq(value, 120000);
+        emit log_named_uint("The value is", value);
+    }
+
+    function testTotalSupply() public {
+        uint totalSupply = ghost.totalSupply();
+        assertEq(totalSupply, 120000*10**18);
+        emit log_named_uint("The total supply is",totalSupply);
+    }
+}
